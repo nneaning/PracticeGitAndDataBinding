0a1165d773088ee2c678107e4191408a7860f252
jinsu4755 <parkjinsu4755@gmail.com>
2020-12-31 13:46:38 +0900

    [Fix] 불필요한 주석 삭제

--- app/src/main/java/sotp/semina/practicegitanddatabinding/BindingAdapter.kt
+++ app/src/main/java/sotp/semina/practicegitanddatabinding/BindingAdapter.kt
@@ -4,10 +4,6 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
-/*TODO step3 아래 BindingAdapter 에서 다음과 같은 일을 처리합니다.
-*  1. 뷰 속성으로 app:image 라는 이름으로 Glide를 사용하여 결합합니다.
-*  이후 다시 레이아웃에서 이미지 부분을 수정합니다.
-*/
 object BindingAdapter {
     @BindingAdapter("app:image")
     @JvmStatic
