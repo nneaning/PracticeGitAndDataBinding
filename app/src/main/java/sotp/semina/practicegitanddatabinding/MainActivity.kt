71864c740d0a3b5be686433d0c27556771d10e78
jinsu4755 <parkjinsu4755@gmail.com>
2020-12-31 13:37:14 +0900

    [Feat] 양방향 데이터 결합 및 버튼 이벤트 결합 추가
    기존에 하지 않으려한 양방향을 observableField로하는 방법으로 추가함

--- app/src/main/java/sotp/semina/practicegitanddatabinding/MainActivity.kt
+++ app/src/main/java/sotp/semina/practicegitanddatabinding/MainActivity.kt
@@ -4,35 +4,29 @@ import android.os.Bundle
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.ObservableField
 import sotp.semina.practicegitanddatabinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+
+    private lateinit var binding: ActivityMainBinding
+    val editText = ObservableField<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.mainActivity = this
         binding.userProfile = UserData(IMAGE_URL, DEFAULT_USER_NAME)
-        addButtonClickEvent(binding)
     }
 
-    private fun setProfileName(binding: ActivityMainBinding, content: String) {
-        binding.userProfile = UserData(IMAGE_URL,content)
+    private fun setProfileName(content: String) {
+        binding.userProfile = UserData(IMAGE_URL, content)
     }
 
-    /*TODO step3 해당 함수를 버튼이 클릭 되었을때 어떤 일이 일어날지 수정합시다.
-    *  필요시 다른 함수를 수정해야함.*/
-    private fun addButtonClickEvent(binding: ActivityMainBinding) {
-        binding.buttonProfileChangeButton.setOnClickListener {
-            setProfileName(
-                binding,
-                getEditTextToString(binding.inputChangedProfile)
-            )
-        }
+    fun changeUserNameEvent() {
+        setProfileName(editText.get() ?: DEFAULT_USER_NAME)
     }
-
-    private fun getEditTextToString(content: EditText): String = content.text.toString()
-
 
     companion object {
         private const val IMAGE_URL =
